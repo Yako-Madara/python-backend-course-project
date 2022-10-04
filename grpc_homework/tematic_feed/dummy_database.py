edfd9aa62c3d86265fa933da6c465743ce349944
Yako-Madara <strelok.onaga@yandex.ru>
2022-10-04 20:10:31 +0300

    modified dummy_database

--- grpc_homework/tematic_feed/dummy_database.py
+++ grpc_homework/tematic_feed/dummy_database.py
@@ -3,18 +3,24 @@ from tematic_feed_pb2 import PostCategory, PostFeed
 # Максимально игрушечная база данных
 posts_by_category = {
     PostCategory.DEVELOPMENT: [
-        PostFeed(id=1, title="The Maltese Falcon"),
-        PostFeed(id=2, title="Murder on the Orient Express"),
-        PostFeed(id=3, title="The Hound of the Baskervilles"),
+        PostFeed(id=1, title="Ускоряем разработку в VSCode"),
+        PostFeed(
+            id=2, title="Интересное о протоколах, сетях и работе интернет-провайдеров"
+        ),
+        PostFeed(id=3, title="Linux Kernel 6.0: что нового «выросло» в ядре?"),
     ],
     PostCategory.SCIENCE_FICTION: [
-        PostFeed(id=4, title="The Hitchhiker's Guide to the Galaxy"),
-        PostFeed(id=5, title="Ender's Game"),
-        PostFeed(id=6, title="The Dune Chronicles"),
+        PostFeed(id=4, title="Cнова про llvm"),
+        PostFeed(id=5, title="Как включить журналы базы данных"),
+        PostFeed(id=6, title="Особенности реализации List в C#"),
     ],
     PostCategory.TESTING: [
-        PostFeed(id=7, title="The 7 Habits of Highly Effective People"),
-        PostFeed(id=8, title="How to Win Friends and Influence People"),
-        PostFeed(id=9, title="Man's Search for Meaning"),
+        PostFeed(
+            id=7, title="GeekUniversity открывает набор на факультет тестирования ПО"
+        ),
+        PostFeed(
+            id=8, title="Яндекс Практикум запускает курс «Инженер по тестированию плюс»"
+        ),
+        PostFeed(id=9, title="Что такое тестирование?"),
     ],
 }
